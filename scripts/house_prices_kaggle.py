64400980acf76a3d8ae49c33d730f1b812cc7162
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2021-10-24 20:00:26 -0500

    Doing the first predictions, not much accurate even the MSE says it is fine

--- scripts/house_prices_kaggle.py
+++ scripts/house_prices_kaggle.py
@@ -1,4 +1,5 @@
 #%% import modules
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -31,9 +32,41 @@ def impute_smart(df):
     return "Imputed done"
 #%%
 impute_smart(train)
-#%%
 impute_smart(test)
 #%%
+train.describe()
+#%%Dummy variables
+train_dummies = pd.get_dummies(train)
+test_dummies = pd.get_dummies(test)
+#%%Split features and variable
+X = train_dummies.drop('SalePrice',axis=1)
+y = train.SalePrice
+#%%Scale data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X)
+X_scaled = scaler.transform(X)
+#%% Split the data
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X_scaled,y,test_size=0.7,random_state=2021)
+#%%Import Models
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+y_pred = lr.predict(X_test)
+#%%Import scores
+from sklearn.metrics import mean_squared_error as MSE
+mse = MSE(y_test,y_pred)**(1/2)
+print("The MSE for the first model is {}".format(mse))
+#%%
+from matplotlib import  pyplot as plt
+plt.plot(y_pred,alpha=0.5)
+plt.plot(y_test,alpha=0.5)
+plt.show()
+
+
+
+
 
 
 
