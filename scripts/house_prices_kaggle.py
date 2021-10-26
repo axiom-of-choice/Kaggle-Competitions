f38f773d5c58c6df88ac58bb7b2d6e5279d8a9f1
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2021-10-26 11:07:58 -0500

    Improving accuracy forcing coefficients be positive for linear regression

--- scripts/house_prices_kaggle.py
+++ scripts/house_prices_kaggle.py
@@ -42,16 +42,16 @@ test_dummies = pd.get_dummies(test)
 X = train_dummies.drop('SalePrice',axis=1)
 y = train.SalePrice
 #%%Scale data
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-scaler.fit(X)
-X_scaled = scaler.transform(X)
+from sklearn.preprocessing import Normalizer
+normalizer = Normalizer()
+normalizer.fit(X)
+X_norm = normalizer.transform(X)
 #%% Split the data
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X_scaled,y,test_size=0.7,random_state=2021)
+X_train,X_test,y_train,y_test = train_test_split(X_norm,y,test_size=0.7,random_state=2021)
 #%%Import Models
 from sklearn.linear_model import LinearRegression
-lr = LinearRegression()
+lr = LinearRegression(positive=True)
 lr.fit(X_train,y_train)
 y_pred = lr.predict(X_test)
 #%%Import scores
@@ -60,10 +60,10 @@ mse = MSE(y_test,y_pred)**(1/2)
 print("The MSE for the first model is {}".format(mse))
 #%%
 from matplotlib import  pyplot as plt
-plt.plot(y_pred,alpha=0.5)
-plt.plot(y_test,alpha=0.5)
+plt.hist(y_pred,alpha=0.5,bins=50)
+plt.hist(y_test,alpha=0.5,bins=50)
+plt.legend(['y_pred','y_test'])
 plt.show()
-
 
 
 
